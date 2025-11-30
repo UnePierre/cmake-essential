61db2d9368783d0c211df957312290b540c89b77
Max Ferger <max.ferger@gmx.de>
2025-12-01 00:05:06 +0100

    feat: resolve_hostname() lookup IP using `dig`

--- CMakeEssentialConfig.cmake
+++ CMakeEssentialConfig.cmake
@@ -1,6 +1,6 @@
 include_guard()
 
-set(CMakeEssential_VERSION 1.2.0)
+set(CMakeEssential_VERSION 1.2.0.1)
 
 
 ####### Expanded from @PACKAGE_INIT@ by configure_package_config_file() #######
---
+++ include/ResolveHostname.cmake
@@ -0,0 +1,33 @@
+include_guard(GLOBAL)
+include("${CMAKE_CURRENT_LIST_DIR}/ParseArguments.cmake")
+
+# `resolve_hostname()`
+#
+# See: https://unix.stackexchange.com/a/20793
+#
+function(resolve_hostname variable_name)
+    parse_arguments(1 "" "" "HOSTNAME")
+
+    if(NOT ${variable_name})
+
+        if(NOT HOSTNAME)
+            set(HOSTNAME "${variable_name}")
+        endif()
+
+        execute_process(
+            COMMAND dig +short ${HOSTNAME}
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            RESULT_VARIABLE ok
+            OUTPUT_VARIABLE output
+            ERROR_VARIABLE error)
+        if(ok EQUAL 0)
+            string(REGEX REPLACE ".*\n" "" output "${output}") # only last line in case there are aliases and/or multiple IPv4/IPv6
+            set(${variable_name} "${output}" PARENT_SCOPE)
+            message(TRACE "resolve_hostname(); ${HOSTNAME} --> ${output}")
+        else()
+            message(FATAL_ERROR "resolve_hostname(): 'dig +short ${HOSTNAME}' unsuccessful! (${error})")
+        endif()
+
+    endif()
+
+endfunction()
